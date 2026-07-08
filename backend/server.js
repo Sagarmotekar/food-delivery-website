391b8953bcfa3ee44b03f63dbe3a6e7ffcc84000
Sagarmotekar <sagarmotekar973@gmail.com>
2026-07-08 10:28:55 +0530

    Fix CORS to allow all origins for Vercel deployments

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,8 @@ const allowedOrigins = [
 //middlewares
 app.use(express.json());
 app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    
+      origin: true, 
     credentials: true
 }));
 
